b8dcc34ac9fe9ee1c49a8adeedfb6fd930503c03
BsdCode <bruno.sdelgado@outlook.com>
2017-06-16 12:18:24 -0300

    Refactor nos Testes e Servidor

--- src/teste/java/br/com/alura/loja/ClienteTest.java
+++ src/teste/java/br/com/alura/loja/ClienteTest.java
@@ -1,21 +1,47 @@
 package br.com.alura.loja;
 
+import java.net.URI;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 import junit.framework.Assert;
 
 public class ClienteTest {
 	
+	private HttpServer server;
+	
+	@Before
+	public void startServer(){
+		server = Servidor.inicializaServidor();
+		
+	}
+	
 	@Test
 	public void testaConexaoComServidor(){
+		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+		WebTarget target = client.target("http://localhost:8080/");
+		String conteudo = target.path("carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+
+	}
+	
+	@After
+	public void killServer(){
+		server.stop();
+	
 	}
 
 }
--- src/teste/java/br/com/alura/loja/ProjetoTeste.java
+++ src/teste/java/br/com/alura/loja/ProjetoTeste.java
@@ -4,16 +4,38 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Projeto;
 
 
 public class ProjetoTeste {
+	private HttpServer server;
+	
+	@Before
+	public void startServer(){
+		server = Servidor.inicializaServidor();
+		
+	}
+	
 	@Test
 	public void testarBuscarProjeto(){
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080/");
 		String conteudo = target.path("projetos").request().get(String.class);
-		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+	
+	@After
+	public void killServer(){
+		server.stop();
+	
 	}
 }
