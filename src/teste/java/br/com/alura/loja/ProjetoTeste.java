64b1e0a47b0a92449e238fc73b5c48180e7f7f04
BsdCode <bruno.sdelgado@outlook.com>
2017-06-16 11:40:47 -0300

    Criado Serviço para busca de Projetos

---
+++ src/teste/java/br/com/alura/loja/ProjetoTeste.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ProjetoTeste {
+	@Test
+	public void testarBuscarProjeto(){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/");
+		String conteudo = target.path("projetos").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+	}
+}
