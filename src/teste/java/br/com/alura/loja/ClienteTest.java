a7e7fdeb61f0fdcd168a09abb53c2f47c258b469
BsdCode <bruno.sdelgado@outlook.com>
2017-06-16 23:37:12 -0300

    Adicionado o metodo POST para salvar entidades

--- src/teste/java/br/com/alura/loja/ClienteTest.java
+++ src/teste/java/br/com/alura/loja/ClienteTest.java
@@ -1,22 +1,22 @@
 package br.com.alura.loja;
 
-import java.net.URI;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
-import junit.framework.Assert;
+import br.com.alura.loja.modelo.Produto;
 
 public class ClienteTest {
 	
@@ -32,10 +32,29 @@ public class ClienteTest {
 	public void testaConexaoComServidor(){
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+		WebTarget target = client.target("http://localhost:8080/");
+		String conteudo = target.path("carrinhos/1").request().get(String.class);
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar",carrinho.getRua());
 
+	}
+	
+	@Test
+	public void testaCriacaoDeCarrinho(){
+		
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        
+		Carrinho carrinho = new Carrinho();
+    	carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+    	carrinho.setRua("Rua Vergueiro");
+    	carrinho.setCidade("Sao Paulo");
+    	String xml = carrinho.toXML();
+    	
+    	 Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+         Response response = target.path("/carrinhos").request().post(entity);
+         Assert.assertEquals("<status>Sucesso</status>", response.readEntity(String.class));
 	}
 	
 	@After
